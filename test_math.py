89dafdc3e2872bf84b678f0daa5274923064eb3d
Gabriel D. Padua <gabriel.d.padua21@gmail.com>
2022-11-07 23:50:00 -0300

    test: 🧪 test fail

--- test_math.py
+++ test_math.py
@@ -5,7 +5,7 @@ class MathTestCase(unittest.TestCase):
 
     def test_sum(self):
         result = sum(10, 10)
-        self.assertEqual(result, 20)
+        self.assertEqual(result, 30)
 
 if __name__=='__main__':
     unittest.main()
