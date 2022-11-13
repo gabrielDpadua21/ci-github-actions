95c975f575b949fbc00ae62cfd0a3f8ab0f2f56a
Gabriel D. Padua <gabriel.d.padua21@gmail.com>
2022-11-13 03:25:13 -0300

    refactor: ♻️ refactor math sun function

---
+++ main.py
@@ -0,0 +1,10 @@
+from math import sum
+
+if __name__=='__main__':
+	try:
+		number1 = int(input("Set a number: "))
+		number2 = int(input("Set other number: "))
+		result = sum(number1, number2)
+		print(result)
+	except:
+		print("Erro to set a number")
